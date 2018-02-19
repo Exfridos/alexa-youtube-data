b43679bf73020ef2c963cf7dbc11b96ee3afe6fa
Exfridos <Exfridos@gmail.com>
2018-02-19 14:56:21 +0100

    Alexa

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const express = require('express')
+const Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.appId = APP_ID // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
+    alexa.execute();
+};
+
+/*const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +15,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
